cb24393e35ff291b47492ead8340b6897ef6970e
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-09-25 14:45:11 +0530

    login successful

--- app/src/main/java/com/rohantaneja/googledrivesample/MainActivity.java
+++ app/src/main/java/com/rohantaneja/googledrivesample/MainActivity.java
@@ -1,14 +1,26 @@
 package com.rohantaneja.googledrivesample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = MainActivity.class.getName();
+    private static final int REQUEST_CODE_RESOLUTION = 1;
     Button buttonGetFiles;
     TextView textViewFileName;
+
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +30,93 @@ public class MainActivity extends AppCompatActivity {
         buttonGetFiles = (Button) findViewById(R.id.b_get_files);
         textViewFileName = (TextView) findViewById(R.id.tv_file_name);
 
+    }
 
+    /**
+     * Called when the activity will start interacting with the user.
+     * At this point your activity is at the top of the activity stack,
+     * with user input going to it.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
 
+            /**
+             * Create the API client and bind it to an instance variable.
+             * We use this instance as the callback for connection and connection failures.
+             * Since no account name is passed, the user is prompted to choose.
+             */
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null) {
+
+            // disconnect Google Android Drive API connection.
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+
+        // Called whenever the API client fails to connect.
+        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+
+        if (!result.hasResolution()) {
+
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
+            return;
+        }
+
+        /**
+         *  The failure has a resolution. Resolve it.
+         *  Called typically when the app is not yet authorized, and an  authorization
+         *  dialog is displayed to the user.
+         */
+
+        try {
+
+            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+
+        } catch (IntentSender.SendIntentException e) {
+
+            Log.e(TAG, "Exception while starting resolution activity", e);
+        }
+    }
+
+    /**
+     * It invoked when Google API client connected
+     *
+     * @param connectionHint
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * It invoked when connection suspended
+     *
+     * @param cause
+     */
+    @Override
+    public void onConnectionSuspended(int cause) {
+
+        Log.i(TAG, "GoogleApiClient connection suspended");
     }
 }
