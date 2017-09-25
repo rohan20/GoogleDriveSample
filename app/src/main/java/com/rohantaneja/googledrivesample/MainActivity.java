ee96cdc0d009c235418900686cc190b60f741b57
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-09-25 14:21:52 +0530

    Added API Key

--- app/src/main/java/com/rohantaneja/googledrivesample/MainActivity.java
+++ app/src/main/java/com/rohantaneja/googledrivesample/MainActivity.java
@@ -2,12 +2,23 @@ package com.rohantaneja.googledrivesample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonGetFiles;
+    TextView textViewFileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonGetFiles = (Button) findViewById(R.id.b_get_files);
+        textViewFileName = (TextView) findViewById(R.id.tv_file_name);
+
+
+
     }
 }
